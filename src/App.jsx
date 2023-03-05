87e2c3c618076e3ffa4c940567efce92e068a5f9
Yuran Simao <yuransimao7@gmail.com>
2023-03-05 17:53:37 +0100

    adding functionality in the add button

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import './App.css'
 
 import itens from './itens'
 
+import { useState } from 'react'
+
 
 
 function App() {
+
+  const [cartItem, setCartItem] = useState([])
  
-  const addItens = (produt) =>{
-    console.log('teste')
+  const addItens = (product) =>{
+   const cart =[ ...cartItem]
+   
+   const ProductExist = cart.find(item => item.id === product.id)
+
+   if(ProductExist){
+    setCartItem(cart.map(items => (
+      items.id === product.id && {...ProductExist, quantidade : ProductExist.quantidade + 1 }
+    )))
+   }
+   else{
+    setCartItem([...cart, {...product , quantidade : 1}])
+   }
+  
   }
+  console.log(cartItem)
 
   return (
     <div className="App">
