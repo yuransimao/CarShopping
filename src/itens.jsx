b281619110c3b8d5c84103f5d1e0ec806bbf51fe
Yuran Simao <yuransimao7@gmail.com>
2023-03-05 14:21:48 +0100

    adding img

---
+++ src/itens.jsx
@@ -0,0 +1,16 @@
+const itens = [
+    {name: "Iphone 12",
+    price: 120000,
+    img : "./assets/iphone-12.jpg"
+},
+    {
+    name: "Iphone 13",
+    price: 150000,
+    img :'./iphone-13.jpg'
+},
+    {
+    name: "Iphone 14",
+    price: 150000,
+    img :'./assets/iphone14-pro.jpg'
+}
+]
