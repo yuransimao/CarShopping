2d21ddc56c16d470645771583ec7c480ad566b0f
Yuran Simao <yuransimao7@gmail.com>
2023-03-05 21:50:17 +0100

    update styles

--- src/Components/Cards/card.jsx
+++ src/Components/Cards/card.jsx
@@ -6,16 +6,15 @@ export function Cards(props){
             <div className="img">
             <img src={props.img} alt={props.name} />
             </div>
-            <div className="title">
+           
+            <div className="Cardbx">
                 <h1>{props.name}</h1>
-            </div>
-            <div>
                 <h2>{props.price}<span>Kz</span></h2>
-            </div>
-
             <div className="btn">
                 <button onClick={() => props.add(props.item)}>Add</button>
             </div>
+            </div>
         </div>
+
     )
 }
