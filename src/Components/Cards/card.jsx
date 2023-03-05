8e174eedf652d3b85fda568fe95207e10110e3c7
Yuran Simao <yuransimao7@gmail.com>
2023-03-05 21:28:03 +0100

    update styling

--- src/Components/Cards/card.jsx
+++ src/Components/Cards/card.jsx
@@ -3,11 +3,11 @@
 export function Cards(props){
     return (
         <div className="Container-card">
-            <div className="title">
-                <h1>{props.name}</h1>
-            </div>
             <div className="img">
             <img src={props.img} alt={props.name} />
+            </div>
+            <div className="title">
+                <h1>{props.name}</h1>
             </div>
             <div>
                 <h2>{props.price}<span>Kz</span></h2>
--- src/itens.jsx
+++ src/itens.jsx
@@ -3,19 +3,19 @@ const itens = [
     id : 1,
     name: "Iphone 12",
     price: 120000,
-    img : "./assets/iphone-12.jpg"
+    img : "../assets/iphone-12.jpg"
 },
     {
     id: 2,
     name: "Iphone 13",
     price: 150000,
-    img :'./iphone-13.jpg'
+    img :'../assets/iphone-13.jpg'
 },
     {
         id: 3,
     name: "Iphone 14",
     price: 150000,
-    img :'./assets/iphone14-pro.jpg'
+    img :'../assets/iphone14-pro.jpg'
 }
 ]
 
