a828e4e7c1b11b2134a238f87f3d5bbce6d97167
Yuran Simao <yuransimao7@gmail.com>
2023-03-05 22:29:26 +0100

    adding navBar

--- src/App.jsx
+++ src/App.jsx
@@ -33,15 +33,17 @@ console.log(cartItem)
 
   return (
     <div className="App">
-      {itens.map((item) => (
-        <Cards key={item.id}
-        name ={item.name}
-        price= {item.price}
-        img={item.img}
-        add={addItens}
-        item={item}
-        />
-      ))}
+      <div className="App-Card">
+        {itens.map((item) => (
+          <Cards key={item.id}
+          name ={item.name}
+          price= {item.price}
+          img={item.img}
+          add={addItens}
+          item={item}
+          />
+        ))}
+      </div>
     </div>
 
   )
---
+++ src/Components/NavBar/navbar.jsx
@@ -0,0 +1,10 @@
+import {BsCart4} from "react-icons/bs"
+export function Navbar (){
+    return(
+        <div className="Navbar">
+            <div className="carrinho">
+                <BsCart4/>
+            </div>
+        </div>
+    )
+}
